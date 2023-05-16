8cedb305c0b3c1ccc84c13c84434a55291705174
Rimil Dey <rimil@appsmith.com>
2023-05-16 19:34:51 +0530

    More styles

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -3,6 +3,7 @@ import Page from '../components/Page';
 export default function myApp({ Component, pageProps }) {
   return (
     <Page>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
       <Component {...pageProps} />
     </Page>
   );
