0d8164d7b09fc52fe5091047b6a75024df010e8b
Rimil Dey <rimil@appsmith.com>
2023-05-22 19:41:13 +0530

    Setup user schema

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import { User } from './schemas/User';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -23,6 +24,8 @@ export default config({
   },
   lists: createSchema({
     // schema items go in here
+    // schema is description of our data would look like, field/rltnships
+    User,
   }),
   ui: {
     // TODO - change this for roles,
